9149544c6b739d4ffece4c153d98ed6740077a47
clexes <mckale@163.com>
2022-04-22 19:08:58 +0800

    test

---
+++ src/com/mk/other/CeShi.java
@@ -0,0 +1,13 @@
+package com.mk.other;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+//动态代理实现InvocationHandler
+public class CeShi implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
