b501cb51b21dafbfa187e9a50fb6681075cfabd5
clexes <mckale@163.com>
2022-05-06 23:52:48 +0800

    ceshi

---
+++ src/com/mk/leetcode/QuickSort.java
@@ -0,0 +1,14 @@
+package com.mk.leetcode;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sum(int[] nums,int ab,int ac){
+        
+        return null;
+    }
+
+}
